9b7b5591e4cce53f7f57d69b17216b7635a38aff
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-14 14:20:24 +0100

    Appwrite DB setup

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,68 @@
+'use server';
+
+import { ID, Query } from 'node-appwrite';
+import { createAdminClient } from '../appwrite';
+import { appwriteConfig } from '../appwrite/config';
+import { parseStringify } from '../utils';
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal('email', [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP(email);
+
+  if (!accountId) throw new Error('Failed to send an OTP');
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCJHYI_IQbqfkPjQMVbxHhqRnTh9xRXGGtYg&s',
+        accountId,
+      }
+    );
+  }
+
+  return parseStringify({ accountId });
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async (email: string) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, 'Faioled to send email OTP');
+  }
+};
