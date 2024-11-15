b1487e28a88e5b69b95fb30e3add9eaa73032b53
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-15 07:56:55 +0100

    Mobile navigation setup started

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React from 'react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import Image from 'next/image';
 const MobileNavigation = () => {
   return (
-    <div>MobileNavigation</div>
-  )
-}
+    <header>
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
 
-export default MobileNavigation
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
+};
+
+export default MobileNavigation;
