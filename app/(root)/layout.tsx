764624a69f0fe511ff807cadc3c2db44fbddb445
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-15 20:03:35 +0100

    Mobile navbar implemented

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,7 +14,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
         <Header />
-        <MobileNavigation />
+        <MobileNavigation {...currentUser} />
         <div className="main-content">{children}</div>
       </section>
     </main>
--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import {
   Sheet,
   SheetContent,
@@ -8,9 +10,27 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet';
 import Image from 'next/image';
-const MobileNavigation = () => {
+import { usePathname } from 'next/navigation';
+import { Separator } from './ui/separator';
+import { navItems } from '@/constants';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import FileUploader from './FileUploader';
+import { Button } from './ui/button';
+
+interface Props {
+  email: string;
+  avatar: string;
+  accountId: string;
+  fullName: string;
+}
+
+const MobileNavigation = ({ accountId, fullName, avatar, email }: Props) => {
+  const [open, setOpen] = useState(false);
+  const pathName = usePathname();
+
   return (
-    <header>
+    <header className="mobile-header">
       <Image
         src="/assets/icons/logo-full-brand.svg"
         alt="logo"
@@ -19,16 +39,77 @@ const MobileNavigation = () => {
         className="h-auto"
       />
 
-      <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="more"
+            width={24}
+            height={24}
+            className="h-auto"
+          />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet h-screen px-3">
+          <SheetTitle>
+            <div className="">
+              <Image
+                src={avatar}
+                alt="avatar"
+                width={44}
+                height={44}
+                className="header-user-avatar"
+              />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
+              </div>
+            </div>
+            <Separator className="mb-4 mt-2 bg-light-200/20" />
+          </SheetTitle>
+
+          <nav>
+            <ul>
+              {navItems.map(({ url, name, icon }) => (
+                <Link key={name} href={url} className="lg:w-full">
+                  <li
+                    className={cn(
+                      'mobile-nav-item',
+                      pathName === url && 'shad-active'
+                    )}
+                  >
+                    <Image
+                      src={icon}
+                      alt={name}
+                      width={24}
+                      height={24}
+                      className={cn(
+                        'nav-icon',
+                        pathName === url && 'nav-icon-active'
+                      )}
+                    />
+                    <p>{name}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <Separator className="mb-4 mt-2 bg-light-200/20" />
+          <div className="flex flex-col justify-betweengap-5">
+            <FileUploader />
+            <Button
+              type="submit"
+              className="mobile-sign-out-button"
+              onClick={() => {}}
+            >
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="logo"
+                width={24}
+                height={24}
+                className="w-6"
+              />
+            </Button>
+          </div>
         </SheetContent>
       </Sheet>
     </header>
