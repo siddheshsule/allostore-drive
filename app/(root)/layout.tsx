b23b78e360e0e0c17d7e8d127e2401fc2f9ce387
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-17 15:08:25 +0100

    Toaster notification added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import Sidebar from '@/components/Sidebar';
 import { getCurrentUser } from '@/lib/actions/user.actions';
 import { redirect } from 'next/navigation';
 import React from 'react';
+import { Toaster } from '@/components/ui/toaster';
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
@@ -13,9 +14,10 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
     <main className="flex h-screen">
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        <Header />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <MobileNavigation {...currentUser} />
         <div className="main-content">{children}</div>
+        <Toaster />
       </section>
     </main>
   );
