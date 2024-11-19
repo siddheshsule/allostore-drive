6c21f1432e45a62ac23e80d1103fb79e05b82388
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-19 18:07:18 +0100

    File size limi updated

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb', // Adjust as needed
+    },
+  },
+
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname:"cdn.pixabay.com",
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
       },
       {
-        protocol: "https",
-        hostname:"cloud.appwrite.io",}
-    ]
-  }
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
